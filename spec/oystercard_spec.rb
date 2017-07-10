c560e984eb1f4748b337660da070e34fd3a68721
Kat Downes <katdownes@Kats-MacBook-Pro.local>
2017-07-10 15:34:15 +0100

    step 5 enabled top up functionality

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -3,10 +3,18 @@ require 'oystercard'
 describe Oystercard do
 
   subject(:card) { described_class.new }
+  ARBITRARY_TOP_UP = 10
 
   describe '#initialize' do
     it 'creates a card with a zero balance' do
       expect(card.balance).to eq 0
+    end
+  end
+
+  describe '#top_up' do
+    it 'tops up the card with the given amount' do
+      card.top_up(ARBITRARY_TOP_UP)
+      expect(card.balance).to eq ARBITRARY_TOP_UP
     end
   end
 
