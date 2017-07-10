5b61b2dd88fb4fd377a7df7dc349bc1fe6ba37e3
Alexander-Blair <zanderb@hotmail.co.uk>
2017-07-10 15:21:44 +0100

    Challenge 4 - card begins with zero balance

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -2,5 +2,12 @@ require 'oystercard'
 
 describe Oystercard do
 
+  subject(:card) { described_class.new }
+
+  describe '#initialize' do
+    it 'creates a card with a zero balance' do
+      expect(card.balance).to eq 0
+    end
+  end
 
 end
