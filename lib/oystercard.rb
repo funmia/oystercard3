4573b886eb67a1b65587ff3cd8cc2ba991c5ab84
Alexander-Blair <zanderb@hotmail.co.uk>
2017-07-10 15:55:06 +0100

    Challenge 6 - maximum balance enforced

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,5 +1,6 @@
-class Oystercard
 
+class Oystercard
+  MAXIMUM_BALANCE = 90
   attr_reader :balance
 
   def initialize
@@ -7,6 +8,13 @@ class Oystercard
   end
 
   def top_up(amount)
+    raise("Maximum limit £#{MAXIMUM_BALANCE} exceeded") if exceeds_limit?(amount)
     @balance += amount
+  end
+
+  private
+
+  def exceeds_limit?(amount)
+    @balance + amount > MAXIMUM_BALANCE
   end
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,7 +1,6 @@
 require 'oystercard'
 
 describe Oystercard do
-
   subject(:card) { described_class.new }
   ARBITRARY_TOP_UP = 10
 
@@ -12,10 +11,14 @@ describe Oystercard do
   end
 
   describe '#top_up' do
+    it 'does not allow card to hold more than the limit' do
+      expect { card.top_up(described_class::MAXIMUM_BALANCE + 1) }
+        .to raise_error("Maximum limit £#{described_class::MAXIMUM_BALANCE} exceeded")
+    end
+
     it 'tops up the card with the given amount' do
       card.top_up(ARBITRARY_TOP_UP)
       expect(card.balance).to eq ARBITRARY_TOP_UP
     end
   end
-
 end
