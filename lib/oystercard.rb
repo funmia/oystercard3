a628e70f538dedfee68eb3785b67ca5dace09a30
oleglukyanov <oleg.s.lukyanov@gmail.com>
2017-07-11 16:20:30 +0100

    Add journey history

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,35 +1,34 @@
-
 class Oystercard
   MAXIMUM_BALANCE = 90
   MINIMUM_FARE = 1
 
-  attr_reader :balance
-  attr_reader :entry_station
+  attr_reader :balance, :entry_station, :journeys
 
   def initialize(balance = 0)
     @balance = balance
-    @entry_station = nil
+    @journeys = []
   end
 
   def top_up(amount)
-    raise("Maximum limit £#{MAXIMUM_BALANCE} exceeded") if exceeds_limit?(amount)
-    @balance += amount
+    limit_error = "Maximum limit £#{MAXIMUM_BALANCE} exceeded"
+    raise(limit_error) if exceeds_limit?(amount)
+    credit(amount)
   end
 
   def touch_in(station)
-    fail("Insufficient funds, please top up") if insufficient_funds?
+    raise('Insufficient funds, please top up') if insufficient_funds?
     in_journey?
     @entry_station = station
   end
 
-  def touch_out
+  def touch_out(station)
     deduct(MINIMUM_FARE)
-    in_journey?
+    add_journey(make_journey(@entry_station, station))
     @entry_station = nil
   end
 
   def in_journey?
-    @entry_station != nil 
+    @entry_station != nil
   end
 
   private
@@ -42,8 +41,19 @@ class Oystercard
     @balance < MINIMUM_FARE
   end
 
+  def credit(amount)
+    @balance += amount
+  end
+
   def deduct(fare)
     @balance -= fare
   end
 
+  def make_journey(entry, exit)
+    { entry => exit }
+  end
+
+  def add_journey(journey)
+    @journeys << journey
+  end
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,69 +1,92 @@
 require 'oystercard'
 
 describe Oystercard do
-  before {@default_balance = 5}
+  before { @default_balance = 5 }
+  before { @minimum_fare = described_class::MINIMUM_FARE }
+  before { @maximum_balance = described_class::MAXIMUM_BALANCE }
+  before { @top_up_cap = @maximum_balance - @default_balance }
+  before { @arbitrary_topup = rand(@minimum_fare..@top_up_cap) }
   subject(:card) { described_class.new(@default_balance) }
-  ARBITRARY_TOP_UP = 10
-  MINIMUM_FARE = 1
-  let(:station) {double :station}
+  let(:station) { double :station }
 
   describe '#initialize' do
     it 'creates a card with a default balance' do
       expect(card.balance).to eq @default_balance
     end
+
+    it 'initializes a card with an empty list of journeys' do
+      expect(card.journeys).to be_empty
+    end
   end
 
   describe '#top_up' do
     it 'does not allow card to hold more than the limit' do
-      expect { card.top_up(described_class::MAXIMUM_BALANCE + 1) }
-        .to raise_error("Maximum limit £#{described_class::MAXIMUM_BALANCE} exceeded")
+      topup = proc { card.top_up(@maximum_balance + 1) }
+      error_message = "Maximum limit £#{@maximum_balance} exceeded"
+      expect { topup.call }.to raise_error(error_message)
     end
 
     it 'tops up the card with the given amount' do
-      card.top_up(ARBITRARY_TOP_UP)
-      expect(card.balance).to eq ARBITRARY_TOP_UP + @default_balance
+      card.top_up(@arbitrary_topup)
+      expect(card.balance).to eq @arbitrary_topup + @default_balance
     end
   end
 
   describe '#touch_in' do
-    it "stores the entry station of the current journey" do
+    it 'stores the entry station of the current journey' do
       card.touch_in(station)
       expect(card.entry_station).to eq station
     end
-    context "insufficient funds" do
-      before {@default_balance = 0}
+
+    context 'insufficient funds' do
+      before { @default_balance = 0 }
       it 'raises an error if the card has insufficient funds' do
-        expect{card.touch_in(station)}.to raise_error("Insufficient funds, please top up")
+        error_message = 'Insufficient funds, please top up'
+        expect { card.touch_in(station) }.to raise_error(error_message)
       end
-    end
-    context "sufficient funds" do
-      before do
-        card.top_up(described_class::MINIMUM_FARE)
-        card.touch_in(station)
-      end
-      it { should be_in_journey }
     end
 
+    context 'sufficient funds' do
+      before do
+        card.top_up(@minimum_fare)
+        card.touch_in(station)
+      end
+
+      it { should be_in_journey }
+    end
   end
 
   describe '#touch_out' do
     before { card.touch_in(station) }
-    it "should not be in journey after touching out" do
-      card.touch_out
-      expect(card.in_journey?).to eq false
+
+    it 'should not be in journey after touching out' do
+      card.touch_out(station)
+      expect(card.in_journey?).to be_falsey
     end
-    it "reduces the balance on touch out by the minimum fare" do
-      expect{card.touch_out}.to change{card.balance}.by(-MINIMUM_FARE)
+
+    it 'reduces the balance on touch out by the minimum fare' do
+      touch_out = proc { card.touch_out(station) }
+      expect { touch_out.call }.to change { card.balance }.by(-@minimum_fare)
     end
-    it "sets the entry station to nil" do
-      card.touch_out
+
+    it 'sets the entry station to nil' do
+      card.touch_out(station)
       expect(card.entry_station).to eq nil
     end
   end
 
   describe '#in_journey?' do
     it 'returns false for a new card' do
-       should_not be_in_journey
+      should_not be_in_journey
+    end
+  end
+
+  context 'after completing the journey' do
+    before { card.touch_in(:waterloo) }
+    before { card.touch_out(:bank) }
+
+    it 'is expected to create one journey' do
+      expect(card.journeys).to eq [{ waterloo: :bank }]
     end
   end
 end
